611a7939e429a9c8cedafa9255fa0a45b6a17374
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-03-04 09:41:38 -0300

    Add files via upload

--- Exercícios - SÁBADO LETIVO/atv01.js
+++ Exercícios - SÁBADO LETIVO/atv01.js
@@ -1,12 +1,11 @@
 let segundos = Number(prompt("Digite o tempo em segundos: "))
-function conversor(seconds) {
-    let horas = Math.floor(seconds / 3600);
+function conversor(seconds,horas,minutos) {
+    horas = Math.floor(seconds / 3600);
     console.log(horas)
-    let minutos = Math.floor((seconds % 3600) / 60);
+    minutos = Math.floor((seconds % 3600) / 60);
     console.log(minutos)
-    let segundosRestantes = seconds % 60;
+    segundosRestantes = seconds % 60;
     console.log(segundosRestantes)
-  
     document.getElementById('info').innerHTML = `${horas}h ${minutos}min ${segundosRestantes}s`
     return `${horas}h ${minutos}min ${segundosRestantes}s`;
   }
