daadcec5a938f422c255057b12bb4374bf714626
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-03-06 10:52:35 -0300

    Delete SABADO LETIVO directory

--- SABADO LETIVO/atv03/atv03.js
+++
@@ -1,8 +0,0 @@
-// Crie uma função que recebe um objeto com o número e porcentagem das
-// propriedades como parâmetros e retorne a porcentagem fornecida do
-// número.
-
-const objeto = {
-    number: parseFloat(prompt("Qual o número?")),
-    porcentage: parseFloat(prompt("Qual a porcengatem desse valor?"))
-}
